d1461d8f12c1655b8ceedf85754375f6fd961a44
mrcx <mrcx.dev@gmail.com>
2024-08-24 19:15:13 -0300

    attribute-models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField(max_length=200)
+    preparation_steps_is_html = models.BooleanField(default=False)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to="recipes/covers/%Y/%m/%d/")
+
+
 # Create your models here.
