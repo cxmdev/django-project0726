81448e5d350fd6294b3a8527260e4cdf277dacf8
mrcx <mrcx.dev@gmail.com>
2024-08-24 18:17:12 -0300

    app-name-and-namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,8 +3,9 @@ from . import (
     views,
 )  # quando se quer importar um  arquivo irmao, ou seja que esta na mesma pasta, basta coocar um . e importar o que se quer.
 
+app_name = "recipes"
 
 urlpatterns = [
-    path("", views.home),
-    path("recipes/<int:id>/", views.recipe),
+    path("", views.home, name="home"),
+    path("recipes/<int:id>/", views.recipe, name="recipe"),
 ]
