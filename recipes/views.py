451f8cc2c347c790724925b85697aab5c0ea421a
Mrcx <mrcx.dev@gmail.com>
2024-08-29 17:25:10 -0300

    404error

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.http import Http404
 
 
 def home(request):
@@ -18,11 +19,16 @@ def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True
     ).order_by("-id")
+
+    if not recipes:
+        raise Http404("Not Found")
+
     return render(
         request,
         "recipes/pages/category.html",
         context={
             "recipes": recipes,
+            "title": f"{recipes.first().category.name} - Category",
         },
     )
 
