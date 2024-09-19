1723c6b23c08c1be7242a8bda53dc8bafd4ac705
Mrcx <mrcx.dev@gmail.com>
2024-09-18 22:27:38 -0300

    django-reverse-pytest-watch-assertion

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import (
     views,
-)  # quando se quer importar um  arquivo irmao, ou seja que esta na mesma pasta, basta coocar um . e importar o que se quer.
+)  # quando se quer importar um  arquivo irmao, ou seja que esta na mesma pasta, basta coocar um .  e importar o que se quer.
 
 app_name = "recipes"
 
