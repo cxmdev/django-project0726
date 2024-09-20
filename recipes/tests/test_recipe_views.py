e071130d2a7f5286003e908b01005d45ee147610
Mrcx <mrcx.dev@gmail.com>
2024-09-20 00:06:58 -0300

    recipes-test

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -25,3 +25,6 @@ class RecipeViewsTest(TestCase):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn('No recipes found here', response.content.decode('utf-8'))
