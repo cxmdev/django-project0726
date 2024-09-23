646a1345db903a206a9b96a758e50ae295ad877e
Mrcx <mrcx.dev@gmail.com>
2024-09-23 17:44:17 -0300

    fixtures-only1-test

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category,Recipe, User
 
 
 class RecipeViewsTest(TestCase):
@@ -20,6 +21,31 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertIn('No recipes found here', response.content.decode('utf-8'))
+    
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title = 'Recipe Title',
+            description = 'Recipe Desciption',
+            slug = 'recipe-slug',
+            preparation_time = '10',
+            preparation_time_unit = 'Minutes',
+            preparation_steps = 'Recipe Preparation Steps',
+            preparation_steps_is_html = 'False',
+            servings = '5',
+            servings_unit = 'pessoas',
+            is_published = 'True',
+        )
+        assert 1 == 1
         
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
