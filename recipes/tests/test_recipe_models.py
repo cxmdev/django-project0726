08734346ee88602acde55fbee8511cb7a143a2e6
Mrcx <mrcx.dev@gmail.com>
2024-10-08 12:59:48 -0300

    .

--- recipes/tests/test_recipe_models.py
+++ recipes/tests/test_recipe_models.py
@@ -1,5 +1,6 @@
 from django.forms import ValidationError
 from .test_recipe_base import RecipeTestBase
+from parameterized import parameterized
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -7,15 +8,28 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_the_test(self):
-        recipe = self.recipe
-        ...
+    # def test_the_test(self):
+    #     recipe = self.recipe
+    #     ...
 
-    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
-        self.recipe.title = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    # def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
+    # self.recipe.title = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 
+    # with self.assertRaises(ValidationError):
+    #     self.recipe.full_clean()
+    # self.recipe.full_clean()  # Aqui a validacao ocorre (nao deixa passar da quantidade de caracteres definidas)
+    # self.recipe.save()
+    # self.fail(len(self.recipe.title))
+
+    @parameterized.expand(
+        [
+            ("title", 65),
+            ("description", 765),
+            ("preparation_time_unit", 65),
+            ("servings_unit", 65),
+        ]
+    )
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, "A" * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
-        # self.recipe.full_clean()  # Aqui a validacao ocorre (nao deixa passar da quantidade de caracteres definidas)
-        # self.recipe.save()
-        # self.fail(len(self.recipe.title))
