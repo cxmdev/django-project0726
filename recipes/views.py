3998383f5173544ff9760deced9eb3f80be30703
mrcx <mrcx.dev@gmail.com>
2024-10-22 23:21:48 -0300

    contains-icontains-q-check-complex-search

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
@@ -43,9 +44,18 @@ def recipe(request, id):
 def search(request):
     search_term = request.GET.get("q", "").strip()
     if not search_term:
-        raise Http404
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) | Q(description__icontains=search_term),
+    ).order_by("-id")
+
     return render(
         request,
         "recipes/pages/search.html",
-        {"page_title": f'Search for"{search_term}" |'},
+        {
+            "page_title": f'Search for"{search_term}" |',
+            "search_term": search_term,
+            "recipes": recipes,
+        },
     )
