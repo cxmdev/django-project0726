0745b2c8fd9dd9a645e2b274e19e3cdf8945b00b
mrcx <mrcx.dev@gmail.com>
2024-11-03 18:11:33 -0300

    template-pagination

--- recipes/views.py
+++ recipes/views.py
@@ -3,21 +3,24 @@ from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
 from django.core.paginator import Paginator
+from utils.pagination import make_pagination_range
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by("-id")
 
-    current_page = request.GET.get("page", 1)
+    try:
+        current_page = int(request.GET.get("page", 1))
+    except ValueError:
+        current_page = 1
     paginator = Paginator(recipes, 9)
-    page_obj = paginator.get_page(1)
+    page_obj = paginator.get_page(current_page)
+    pagination_range = make_pagination_range(paginator.page_range, 4, current_page)
 
     return render(
         request,
         "recipes/pages/home.html",
-        context={
-            "recipes": page_obj,
-        },
+        context={"recipes": page_obj, "pagination_range": pagination_range},
     )
 
 
